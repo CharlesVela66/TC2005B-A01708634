76cc20ace849f8748d14cc87717576fc1ab0c9eb
Carlos Velasco <A01708634@tec.mx>
2023-03-06 11:32:04 -0600

    Mar 6 2.0

--- Lab 12/routes/futbol.routes.js
+++ Lab 12/routes/futbol.routes.js
@@ -4,45 +4,17 @@ const filesystem = require('fs');
 const router = express.Router();
 
 router.get('/', (request, response, next) => {
-    let html =`
-        <h1> Has elegido registrar un jugador de futbol </h1>
-        <a href = "/futbol/nuevo">
-            <button>
-                Registrar
-            </button>
-        </a>
-    `;
-    response.send(html);
+    response.render("futbol");
 });
 
 router.get('/nuevo', (request, response, next) => {
-    let html = `
-        <form action = "/futbol/nuevo" method = "POST">
-            <label for "player"> Nombre del jugador </label>
-            <input type = "text" id = "player" name = "player">
-            <input type = "submit" value = "Enviar">
-        </form>
-    `;
-    response.send(html);
+    response.render("futbolista_nuevo");
 });
 router.post('/nuevo', (request, response, next) => {
     let player =  request.body.player;
-    let html = `
-        El jugador de futbol es: 
-    `
-    html += player;
-    html += `
-    <br> <br>
-    <a href = "/home">
-        <button>
-            Volver a home
-        </button>
-    </a>
-    `;
     filesystem.writeFileSync("futbolista.txt", "El nombre del futbolista es: " + player);
-    console.log(request.body);
-    console.log(request.body.player);
-    response.send(html);
+    console.log(player);
+    response.render("mostrar_futbolista", {player : player});
 });
 
 module.exports = router;
--- Lab 12/routes/hockey.routes.js
+++ Lab 12/routes/hockey.routes.js
@@ -4,45 +4,16 @@ const filesystem = require('fs');
 const router = express.Router();
 
 router.get('/', (request, response, next) => {
-    let html =`
-        <h1> Has elegido registrar un jugador de hockey </h1>
-        <a href = "/hockey/nuevo">
-            <button>
-                Registrar
-            </button>
-        </a>
-    `;
-    response.send(html);
+    response.render("hockey");
 });
 
 router.get('/nuevo', (request, response, next) => {
-    let html = `
-        <form action = "/hockey/nuevo" method = "POST">
-            <label for "player"> Nombre del jugador </label>
-            <input type = "text" id = "player" name = "player">
-            <input type = "submit" value = "Enviar">
-        </form>
-    `;
-    response.send(html);
+    response.render("hockey_nuevo");
 });
 router.post('/nuevo', (request, response, next) => {
     let player =  request.body.player;
-    let html = `
-        El jugador de hockey es: 
-    `
-    html += player;
-    html += `
-    <br> <br>
-    <a href = "/home">
-        <button>
-            Volver a home
-        </button>
-    </a>
-    `;
     filesystem.writeFileSync("jugador-hockey.txt", "El nombre del jugador de hockey es: " + player);
-    console.log(request.body);
-    console.log(request.body.player);
-    response.send(html);
+    response.render("mostrar_hockey",{player:player});
     
 });
 
