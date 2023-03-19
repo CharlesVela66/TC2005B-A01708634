66d0a7537b8350a98fa6b901f26f8b0629c51994
Carlos Velasco <A01708634@tec.mx>
2023-03-18 21:22:01 -0600

    Mar 18

---
+++ Lab 18/models/bitacora.model.js
@@ -0,0 +1,30 @@
+const db = require('../util/database');
+
+module.exports = class Bitacora {
+
+    //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
+    constructor(nuevo_registro) {
+        this.descr_sesion = nuevo_registro.descr_sesion || null;
+        this.nivel_satisf = nuevo_registro.nivel_satisf || 1;
+        this.comentarios = nuevo_registro.comentarios || "No agregó comentarios";
+    }
+
+    //Este método servirá para guardar de manera persistente el nuevo objeto. 
+    save() {
+        return db.execute(`
+            INSERT INTO bitacora (id_cliente, id_rutina, nivel_satisf, descripcion_sesion, comentarios)
+            VALUES (?, ?, ?, ?, ?)
+        `, [1, 1, this.nivel_satisf, this.descr_sesion, this.comentarios]);
+    }
+
+    //Este método servirá para devolver los objetos del almacenamiento persistente.
+    static fetchAll() {
+        return db.execute(`
+            SELECT fecha, nivel_satisf, descripcion_sesion, comentarios
+            FROM bitacora 
+            WHERE id_cliente = 1
+            ORDER BY fecha DESC;
+        `);
+    }
+
+}
---
+++ Lab 18/routes/home.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+const homeController = require('../controllers/home.controller');
+
+router.get('/iniciar-sesion', homeController.iniciar_sesion);
+
+router.post('/iniciar-sesion', homeController.post_iniciar_sesion)
+
+router.get('/registrarse', homeController.registrarse);
+
+router.post('/registrarse', homeController.post_registrarse);
+
+router.get('/', homeController.inicio);
+
+module.exports = router;
